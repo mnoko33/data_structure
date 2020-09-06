4a2b8a820e606b8d6a138090205f47d43b2c9347
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-09-06 17:54:21 +0900

    퀵소트

--- sort/Quick.js
+++ sort/Quick.js
@@ -1,3 +1,23 @@
 const quickSort = arr => {
-  
+  const sort = (left, right) => {
+    if (right - left < 1) return;
+
+    const pivotIdx = parseInt((left + right) / 2);
+    const pivot = arr[pivotIdx]
+    low = left;
+    high = right;
+    while (low < high) {
+      while (arr[low] < pivot && low < right) low++;
+      while (pivot < arr[high] && left < high) high--;
+
+      if (low >= high) break;
+      [arr[low], arr[high]] = [arr[high], arr[low]]
+      low++;
+      high--;
+    }
+    sort(left, high);
+    sort(high+1, right);
+  }
+
+  sort(0, arr.length-1);
 }
